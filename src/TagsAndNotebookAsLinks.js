78c60c76aeb904ce648bf1e05bc2f44e9a68ae03
ambrt <ambrth@gmail.com>
2021-01-01 13:58:19 +0100

    markdown links

---
+++ src/TagsAndNotebookAsLinks.js
@@ -0,0 +1,112 @@
+function plugin(markdownIt, _options) {``
+    var defaultRender = markdownIt.renderer.rules.link_open;
+    markdownIt.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+        var token = tokens[idx];
+        console.log(token)
+        console.info(token)
+        aIndex = token.attrIndex('href');
+        console.log(aIndex)
+        if(token.attrs[aIndex][1].substr(0,3)==":/#"){
+            
+            //token.attrs.push(['js', "webviewApi.executeCommand('testCommand', 'one', 'two'); return false;"]);
+            let resourceIdAttr = '';
+            let icon = '';
+            let hrefAttr = '#';
+            let mime = '';
+            let resourceId = '';
+            let resource = null;
+            let js = `onclick="webviewApi.executeCommand('goToTag', '${token.attrs[aIndex][1].substr(3)}'); return false;"`;
+            const attrHtml = [];
+            attrHtml.push(`href='${hrefAttr}'`);
+            if (js) attrHtml.push(js);
+            
+            let replacement={html:`<a ${attrHtml.join(' ')}>`,}
+
+            _options.context.currentLinks.push({
+                href: "#",
+                resource: resource,
+                resourceReady: true,
+                resourceFullPath: null
+            });
+    
+            return replacement.html;
+            
+            
+        }
+        if(token.attrs[aIndex][1].substr(0,3)==":/@"){
+            
+            //token.attrs.push(['js', "webviewApi.executeCommand('testCommand', 'one', 'two'); return false;"]);
+            let resourceIdAttr = '';
+            let icon = '';
+            let hrefAttr = '#';
+            let mime = '';
+            let resourceId = '';
+            let resource = null;
+            let js = `onclick="webviewApi.executeCommand('goToNotebook', '${token.attrs[aIndex][1].substr(3)}'); return false;"`;
+            const attrHtml = [];
+            attrHtml.push(`href='${hrefAttr}'`);
+            if (js) attrHtml.push(js);
+            
+            let replacement={html:`<a ${attrHtml.join(' ')}>`,}
+
+            _options.context.currentLinks.push({
+                href: "#",
+                resource: resource,
+                resourceReady: true,
+                resourceFullPath: null
+            });
+    
+            return replacement.html;
+            
+            
+        }
+
+
+        //     return '<div class="embed-responsive embed-responsive-16by9">\n' +
+        //            '  <iframe class="embed-responsive-item" src="//player.vimeo.com/video/' + id + '"></iframe>\n' +
+        //            '</div>\n';
+        //   }
+        
+        // if (vimeoRE.test(token.attrs[aIndex][1])) {
+	
+        //     var id = token.attrs[aIndex][1].match(vimeoRE)[2];
+        
+        //     return '<div class="embed-responsive embed-responsive-16by9">\n' +
+        //            '  <iframe class="embed-responsive-item" src="//player.vimeo.com/video/' + id + '"></iframe>\n' +
+        //            '</div>\n';
+        //   }
+        
+        return defaultRender(tokens, idx, options, env, self);
+
+    }
+
+    // console.log('asdf')
+    //     console.info('asdf')
+	// const defaultRender = markdownIt.renderer.rules.fence || function(tokens, idx, options, env, self) {
+	// 	return self.renderToken(tokens, idx, options, env, self);
+	// };
+
+	// markdownIt.renderer.rules.fence = function(tokens, idx, options, env, self) {
+    //     const token = tokens[idx];
+    //     console.log(token)
+    //     console.info(token)
+    //     // if (token.info !== 'justtesting') return defaultRender(tokens, idx, options, env, self);
+        
+	// 	return `
+	// 		<div class="just-testing">
+	// 			<p>JUST TESTING: ${token.content}</p>
+	// 			<p><a href="#" onclick="webviewApi.executeCommand('testCommand', 'one', 'two'); return false;">Click to send "testCommand" to plugin</a></p>
+	// 			<p><a href="#" onclick="webviewApi.executeCommand('testCommandNoArgs'); return false;">Click to send "testCommandNoArgs" to plugin</a></p>
+	// 		</div>
+	// 	`;
+	// };
+}
+
+module.exports = {
+	default: function(_context) { 
+		return {
+			plugin: plugin,
+			
+		}
+	},
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import joplin from 'api';
-import { MenuItemLocation, ToolbarButtonLocation } from 'api/types';
+import { MenuItemLocation, ToolbarButtonLocation, ContentScriptType } from 'api/types';
 
 
 function escapeTitleText(text: string) {
@@ -8,6 +8,60 @@ function escapeTitleText(text: string) {
 
 joplin.plugins.register({
 	onStart: async function () {
+		async function findTag(tagName) {
+			console.log(tagName)
+			let tagId = ''
+			let page = 1
+			let tags
+			let has_more = true
+			while (has_more) {
+				tags = await joplin.data.get(['tags'], { page: page });
+
+				for (let i = 0; i < tags.items.length; i++) {
+					let element = tags.items[i]
+					console.log(element)
+					if (element.title == tagName) {
+						console.log('open ' + element.id)
+						tagId = element.id
+						return tagId
+					}
+				}
+				/*tags.items.forEach(element => {
+					console.log(element)
+					if (element.title==tagName){
+						console.log('open '+element.id)
+						tagId = element.id
+						return tagId
+					}
+				});
+				*/
+				if (tags.has_more) { page = page + 1 } else { has_more = false }
+
+			}
+
+		}
+		async function findNotebook(notebookName) {
+			let notebookId = ''
+			let page = 1
+			let folders
+
+			let has_more = true
+			while (has_more) {
+				folders = await joplin.data.get(['folders'], { page: page });
+
+
+				for (let i = 0; i < folders.items.length; i++) {
+					let element = folders.items[i]
+					console.log(element)
+					if (element.title == notebookName) {
+						console.log('open ' + element.id)
+						notebookId = element.id
+						return notebookId
+					}
+				} if (folders.has_more) { page = page + 1 } else { has_more = false }
+
+			}
+		}
 		await joplin.commands.register({
 			name: 'goToItemViaText',
 			label: 'Go to note via highlighted text',
@@ -17,75 +71,106 @@ joplin.plugins.register({
 				const selectedText = (await joplin.commands.execute('selectedText') as string);
 
 				try {
-					let first = selectedText.substr(0,1);
-					if(first == "#"){
-						let tagId =''
-						let page = 1
-						let tags
-						console.info(tags);
-						let has_more = true
-						while(has_more){
-						tags = await joplin.data.get(['tags'], {page:page});
-						tags.items.forEach(element => {
-							console.log(element)
-							if (element.title==selectedText.substr(1)){
-								console.log('open '+element.id)
-								tagId = element.id
-							}
-						});
-						if(tags.has_more) {page=page+1} else {has_more=false}
-						
+					let first = selectedText.substr(0, 1);
+					if (first == "#") {
+
+						let tagId = await findTag(selectedText.substr(1))
+
+						await joplin.commands.execute('openTag', tagId);
+
 					}
-							
-							
-						await joplin.commands.execute('openTag',tagId);		
-						
-						
+					else if (first == "@") {
+
+						let folderId = await findNotebook(selectedText.substr(1))
+						//console.info(folders);
+						await joplin.commands.execute('openFolder', folderId);
+
+
 					}
-					else if(first =="@"){
-						let folderId =''
-						let page = 1
-						let folders
-						
-						let has_more = true
-						while(has_more){
-						folders = await joplin.data.get(['folders'], {page:page});
-						folders.items.forEach(element => {
-							console.log(element)
-							if (element.title==selectedText.substr(1)){
-								console.log('open '+element.id)
-								folderId = element.id
-							}
-						});
-						if(folders.has_more) {page=page+1} else {has_more=false}
-						
+					else {
+						await joplin.commands.execute('openNote', selectedText);
 					}
-						
-					console.info(folders);
-					await joplin.commands.execute('openFolder',folderId);		
-						
-						
-					}
-					else{
-						await joplin.commands.execute('openNote',selectedText);
-					}
-				
+
 				}
-				catch(err)
-				{	
+				catch (err) {
 					console.log(err)
-					let dialog = await joplin.views.dialogs.create('wrongText')					
+					let dialog = await joplin.views.dialogs.create('wrongText')
 					await joplin.views.dialogs.setHtml(dialog, "Seems like text is not an exact id,tag or folder<br>Or other error occured");
-					await joplin.views.dialogs.setButtons(dialog,[{id:"ok"}]);
+					await joplin.views.dialogs.setButtons(dialog, [{ id: "ok" }]);
 					await joplin.views.dialogs.open(dialog);
 				}
-				
+
 
 			}
 		})
+
+		await joplin.commands.register({
+			name: 'convertTextToItem',
+			label: 'Converts text to tag or notebook',
+			iconName: 'fas fa-anchor',
+			execute: async () => {
+				//Get selected text
+				const selectedText = (await joplin.commands.execute('selectedText') as string);
+
+				try {
+					let first = selectedText.substr(0, 1);
+					if (first == "#") {
+
+						let tagId = await findTag(selectedText.substr(1))
+
+						await joplin.commands.execute('replaceSelection', `[${escapeTitleText(selectedText)}](:/#${tagId})`);
+
+					}
+					else if (first == "@") {
+
+						let notebookId = await findNotebook(selectedText.substr(1))
+
+						await joplin.commands.execute('replaceSelection', `[${escapeTitleText(selectedText)}](:/@${notebookId})`);
+
+
+					}
+					else {
+						await joplin.commands.execute('openNote', selectedText);
+					}
+
+				}
+				catch (err) {
+					console.log(err)
+					let dialog = await joplin.views.dialogs.create('wrongText')
+					await joplin.views.dialogs.setHtml(dialog, "Seems like text is not an exact id,tag or folder<br>Or other error occured");
+					await joplin.views.dialogs.setButtons(dialog, [{ id: "ok" }]);
+					await joplin.views.dialogs.open(dialog);
+				}
+
+
+			}
+		})
+
 		await joplin.views.menuItems.create('goToViaTextMenuItem', 'goToItemViaText', MenuItemLocation.EditorContextMenu);
-		await joplin.views.toolbarButtons.create('Go to item via text', 'goToItemViaText',ToolbarButtonLocation.EditorToolbar);
-		
+		await joplin.views.menuItems.create('convertTextToItemMenuItem', 'convertTextToItem', MenuItemLocation.EditorContextMenu);
+		await joplin.views.toolbarButtons.create('Go to item via text', 'goToItemViaText', ToolbarButtonLocation.EditorToolbar);
+
+		await joplin.commands.register({
+			name: 'goToTag',
+			label: 'Go to tag from render view',
+			execute: async (...args) => {
+				//alert('Going to tag' + JSON.stringify(args));
+				await joplin.commands.execute('openTag', args[0]);
+			},
+		});
+		await joplin.commands.register({
+			name: 'goToNotebook',
+			label: 'Got to notebook from render view',
+			execute: async (...args) => {
+				await joplin.commands.execute('openFolder', args[0]);
+			},
+		});
+		await joplin.plugins.registerContentScript(
+			ContentScriptType.MarkdownItPlugin,
+			'tagsandnotebookaslinks',
+			'./TagsAndNotebookAsLinks.js'
+		);
+
 	}
 
 });
